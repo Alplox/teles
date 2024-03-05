9bdd8302756a688319e101a57a680ef42d69a807
reneantonio <rene.antonio.alvarez@gmail.com>
2024-03-05 12:58:53 -0300

    Update canales.js add elconquistadorfm.net
    add elconquistadorfm.net

--- assets/js/canales.js
+++ assets/js/canales.js
@@ -253,6 +253,13 @@ const listaCanales = {
         'pais': 'cl',
         'alt_icon': '<i class="bi bi-boombox"></i>'
     },
+    'radio-el-conquistador': {
+        'nombre': 'Radio El Conquistador FM',
+        'iframe_url': 'https://redirector.rudo.video/hls-video/931b584451fa6dd1313ee66efbfd5802e3f3bcea/elconquistadortv/elconquistadortv.smil/playlist.m3u8',
+        'fuente': 'https://www.elconquistadorfm.net/',
+        'pais': 'cl',
+        'alt_icon': '<i class="bi bi-boombox"></i>'
+    },
     'radio-folclor-chile': {
         'nombre': 'Radio Folclor de Chile',
         'yt_id': 'UC0Hl8kJe8Xwv8g63Q4qefQg',
